e006be0b54f249bc102e3e9299aa46011613d21a
Ben Holt <ben.holt@bc.libraries.coop>
2017-11-02 15:11:38 -0700

    Add the toolshed_support module, this time properly and not as a git submodule

---
+++ sites/all/modules/toolshed_support/js/toolshed_support_select2.js
@@ -0,0 +1,13 @@
+/*
+ *  * Initialise select2 for form elements
+ *   */
+
+(function ($, Drupal) {
+
+  'use strict';
+  /* CODE GOES HERE */
+  $(document).ready(function() {
+    $('.facetapi-multiselect').select2();
+  });
+
+})(jQuery, Drupal);
