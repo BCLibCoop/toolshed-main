c6b1c8539dc86adb46002a02073b9cb2c516e86f
Ben Holt <ben.holt@bc.libraries.coop>
2017-11-02 14:54:16 -0700

    Add toolshed_support module and toolshed theme

---
+++ sites/all/modules/toolshed_support/js/toolshed_support_chosen.js
@@ -0,0 +1,13 @@
+/*
+ *  * Initialise chosen for form elements
+ *   */
+
+(function ($, Drupal) {
+
+  'use strict';
+  /* CODE GOES HERE */
+  $(document).ready(function() {
+    $(".facetapi-multiselect-field-primary-audience").chosen()
+  });
+
+})(jQuery, Drupal);
